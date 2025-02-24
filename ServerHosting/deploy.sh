a424295b86e06b61bdb1eb928c1e8d14a9be4070
Joseph Baruch <josephbaruch48@gmail.com>
2025-02-23 20:22:47 -0800

    change pathing

--- ServerHosting/deploy.sh
+++ ServerHosting/deploy.sh
@@ -6,6 +6,6 @@ kubectl delete service josephbaruch-service --ignore-not-found
 kubectl delete ingress josephbaruch-ingress --ignore-not-found
 
 # Apply the new deployment, service, and ingress configurations
-kubectl apply -f /Users/joseph.baruch/REPO/personal/joseph-baruch.com/ServerHosting/deployment.yaml
-kubectl apply -f /Users/joseph.baruch/REPO/personal/joseph-baruch.com/ServerHosting/service.yaml
-kubectl apply -f /Users/joseph.baruch/REPO/personal/joseph-baruch.com/ServerHosting/ingress.yaml
+kubectl apply -f ./deployment.yaml
+kubectl apply -f ./service.yaml
+kubectl apply -f ./ingress.yaml
